5093e6470bcccd2a5e6fcab087ff5967a6cead2d
jrice189 <jrice189@gmail.com>
2016-04-30 15:38:09 +0000

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.json('i am a beautiful butterfly');
+  res.json({'i am a beautiful butterfly':true});
 });
 
 app.listen(process.env.PORT || 4730);

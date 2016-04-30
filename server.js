5f4659df2c03363468f16f0b0c4fba0bb4da28d2
jrice189 <jrice189@gmail.com>
2016-04-30 22:12:33 +0000

    Return selections from request header

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.json(req.headers);
+  var output = {
+      "ipaddress":req.headers["x-forwarded-for"],
+      "language":req.headers["accept-language"],
+      "software":req.headers["user-agent"].slice(req.headers["user-agent"].indexOf("(")+1,req.headers["user-agent"].indexOf(")"))
+  }
+  res.json(output);
 });
 
 app.listen(process.env.PORT || 4730);
